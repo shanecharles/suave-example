b2d7b6e57da0613c5506367974455261ed8d0c50
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-27 18:25:51 -0600

    Basic getting of open bugs and individual bugs.

--- app.fsx
+++ app.fsx
@@ -1,16 +1,37 @@
 #r "packages/Suave/lib/net40/Suave.dll"
+#r "packages/NewtonSoft.Json/lib/net45/newtonsoft.json.dll"
+#load "db.fsx"
 
 open Suave
 open Suave.Operators  // Fish operator >=>
 open Suave.Filters    // GET, POST, Put, ...
 open Suave.Successful
+open System
+open BugDb.Access
+
+open Newtonsoft.Json
 
 let serverDateTime = warbler (fun _ -> System.DateTime.UtcNow.ToString() |> OK)
+
+let jsonMime = Writers.setMimeType "application/json"
+let getOpenBugs = (fun x -> 
+                    async { 
+                        let! bugs = AsyncGetOpenBugs
+                        return! OK (bugs |> JsonConvert.SerializeObject) x
+                    })
+
+let bugDetails id = 
+    match AsyncGetBug id |> Async.RunSynchronously with 
+    | [] -> id |> sprintf "Bug id %d is not found." |> RequestErrors.NOT_FOUND
+    | h :: _ -> OK (JsonConvert.SerializeObject(h))
+
 
 let app = 
     choose
       [ GET >=> choose 
             [ path "/" >=> OK "Faster APIs with Suave.IO"
+              path "/api/bugs/open" >=> jsonMime >=> getOpenBugs
+              pathScan "/api/bugs/%d" bugDetails
               path "/api/time" >=> serverDateTime ]]
 
 startWebServer defaultConfig app
---
+++ db.fsx
@@ -0,0 +1,34 @@
+﻿namespace BugDb
+
+module Db =
+
+    open System
+
+    type Bug = { Id : int; Details : string; Closed : DateTime option }
+
+    type DbQuery = 
+        | OpenBugs of AsyncReplyChannel<Bug list>
+        | Bug of AsyncReplyChannel<Bug list> * int
+
+    let db = MailboxProcessor.Start(fun inbox -> 
+        let rec loop ((id, bugs) as oldState) = 
+            async {
+                let! msg = inbox.Receive ()
+                let newState = 
+                    match msg with
+                    | OpenBugs c -> 
+                        c.Reply (bugs)
+                        oldState
+                    | Bug (c, id) ->
+                        c.Reply (bugs |> List.filter (fun {Id = id'} -> id = id'))
+                        oldState
+                return! loop newState
+            }
+        loop (2,[{Id = 1; Details = "Nothing works"; Closed = None}])
+        )
+
+module Access = 
+    open Db
+
+    let AsyncGetOpenBugs = Db.db.PostAndAsyncReply(fun c -> OpenBugs c)
+    let AsyncGetBug id = Db.db.PostAndAsyncReply(fun c -> Bug (c,id))
